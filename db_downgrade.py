449b12e0da731941acd013aaf7714df962b84bb8
jcools <jcools@nate.com>
2015-09-23 23:42:12 +0900

    db_migration add

---
+++ db_downgrade.py
@@ -0,0 +1,10 @@
+#db_downgrade.py
+
+from migrate.versioning import api
+from config import SQLALCHEMY_DATABASE_URI
+from config import SQLALCHEMY_MIGRATE_REPO
+
+v = api.db_version(SQLALCHEMY_DATABASE_URI,SQLALCHEMY_MIGRATE_REPO)
+api.downgrade(SQLALCHEMY_DATABASE_URI,SQLALCHEMY_MIGRATE_REPO,v-1)
+
+print('Current database version:'+str(v))
